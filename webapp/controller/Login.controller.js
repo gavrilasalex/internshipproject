4d52e1a3279cab391b8a84c00127d707742b981d
Andreea Coaja <andreea.coaja@mhp.com>
2020-08-03 16:43:25 +0300

    Styling

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -4,6 +4,11 @@ sap.ui.define([
  ], function (BaseController) {
     "use strict";
     return BaseController.extend("intern2020.controller.Login", {
-       
+      onLoginPress : function (oEvent) {
+         this.email=this.byId('emailInput').getValue();
+         this.password=this.byId('passwordInput').getValue();
+      var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+      oRouter.navTo("main");
+     },
     });
  });
